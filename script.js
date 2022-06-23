3a52465a20bbd1724bd9961cbd1dc5b8852dafef
Елизавета Наборщикова <lisa_redfox@mail.ru>
2022-06-24 01:35:47 +0500

    Закончена работа

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-fetch('https://api.github.com/users/lisaredfox').then {res => res.json()}.then {json => console.log(json)}
+
+fetch('https://api.github.com/users/lisaredfox')
+    .then (res => res.json())
+    .then (json => {
+        let photo = json.avatar_url;
+        let name = json.name;
+        let description = json.bio;
+        let body = document.getElementsByTagName('body');
+        let picture = document.createElement('img');
+        document.body.append(picture);
+        picture.src = photo;
+
+        let wrapper = document.createElement('p');
+        document.body.append(wrapper);
+        let userName = document.createElement('a');
+        userName.href = json.html_url;
+        wrapper.append(userName);
+         if (name == null){
+            userName.innerHTML = 'Elizaveta Naborschikova'}
+            else if (name == undefined){
+                userName.innerHTML = 'Elizaveta Naborschikova'}
+            else {userName.innerHTML = name};            
+
+            let userDescription = document.createElement('p');
+        document.body.append(userDescription);
+         if (description == null){
+            userDescription.innerHTML = 'ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ НЕДОСТУПНА'}
+            else if (description == undefined){
+                userDescription.innerHTML = 'ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ НЕДОСТУПНА'}
+            else {userDescription.innerHTML = description};
+    })
